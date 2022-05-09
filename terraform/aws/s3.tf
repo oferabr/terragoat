9045d9446c1e866b1f3f7fa8c5a4abdda08262ae
oferabr <42511847+oferabr@users.noreply.github.com>
2022-05-09 22:05:02 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -41,6 +41,7 @@ resource "aws_s3_bucket_object" "data_object" {
 }
 
 resource "aws_s3_bucket" "financials" {
+  
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
